188d543fff8634b831bd9def4bf059b71b5e2f0c
Samuel Lucky <samuelnoye35@gmail.com>
2025-06-02 15:55:48 +0100

    working on the auth logic

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express  from "express";
+import { json } from "body-parser";
+import {autRoutes} from "routes/auth_routes"
+
+const app = express()
+
+app.use(json())
+
+app.use('/auth', autRoutes)
